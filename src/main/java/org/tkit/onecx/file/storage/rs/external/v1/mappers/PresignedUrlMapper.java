d629f3d82f7e903f8258ff319efc22f49482ef39
dpylak <203235923+dawpyl@users.noreply.github.com>
2026-03-11 10:14:02 +0100

    fix: fix issue with an error during generating presigned url

--- src/main/java/org/tkit/onecx/file/storage/rs/external/v1/mappers/PresignedUrlMapper.java
+++ src/main/java/org/tkit/onecx/file/storage/rs/external/v1/mappers/PresignedUrlMapper.java
@@ -1,12 +1,14 @@
 package org.tkit.onecx.file.storage.rs.external.v1.mappers;
 
+import java.time.OffsetDateTime;
+
 import org.mapstruct.Mapper;
 import org.tkit.quarkus.rs.mappers.OffsetDateTimeMapper;
 
 import gen.org.tkit.onecx.file.storage.rs.external.v1.model.PresignedUrlResponseDTOV1;
 
 @Mapper(uses = OffsetDateTimeMapper.class)
-public interface PresginedUrlMapper {
+public interface PresignedUrlMapper {
 
-    PresignedUrlResponseDTOV1 map(String url, String expiration);
+    PresignedUrlResponseDTOV1 map(String url, OffsetDateTime expiration);
 }
